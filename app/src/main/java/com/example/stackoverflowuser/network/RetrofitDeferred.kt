4ec870058d5329eeee26e0783787b214d5167079
dinhtho <tho.nguyen@beesightsoft.com>
2019-05-13 17:52:23 +0700

    remove unused code

--- app/src/main/java/com/example/stackoverflowuser/network/RetrofitDeferred.kt
+++
@@ -1,9 +0,0 @@
-package com.example.stackoverflowuser.network
-
-import kotlinx.coroutines.Deferred
-
-/**
- * Created by tho nguyen on 2019-05-11.
- */
-interface RetrofitDeferred<T>:Deferred<T> {
-}
